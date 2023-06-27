9414375fc5baa7e1e6cc16e77c814ce082471f3a
VirusNinja <lesco.kendy@gmail.com>
2023-06-27 20:46:38 +0900

    hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle")
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    // mumbai: {
+    //   url: "https://rpc-mumbai.maticvigil.com",
+    //   accounts: [process.env.privateKey]
+    // }
+  },
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
